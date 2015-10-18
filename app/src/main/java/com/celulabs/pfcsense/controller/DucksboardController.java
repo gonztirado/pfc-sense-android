87759a3d0fdf8b68961d82e2628055e89b30873e
Gonz Tirado <gtirado@movion.es>
2015-10-18 18:41:06 +0200

    PFC-39 - Controlador de Ducksboard
    Se ha incluido un control de tiempo en el envío de datos al dashboard, 3" para los valores, 60" para las gráficas

--- app/src/main/java/com/celulabs/pfcsense/controller/DucksboardController.java
+++ app/src/main/java/com/celulabs/pfcsense/controller/DucksboardController.java
@@ -14,26 +14,42 @@ import org.json.JSONObject;
  */
 public class DucksboardController {
 
-    public static final MediaType JSON
-            = MediaType.parse("application/json; charset=utf-8");
-    public static final String API_KEY = "dUpDbnYxWG1hdlFPejVBUXhPaXBrTXpBd1phbzBXeUZ4VlNlamludXhqQnFpZnplMEQ6Z2FuenUxM0dU";
-    public static final String JSON_PARAM_TIMESTAMP = "timestamp";
-    public static final String JSON_PARAM_VALUE = "value";
-    public static final String API_PUSH_URL = "https://push.ducksboard.com/v/";
-    public static final String WIDGET_VALUE_TEMPERATURE = "730979";
-    public static final String WIDGET_VALUE_TEMPERATURE_IR = "730980";
-    public static final String WIDGET_VALUE_HUMIDITY = "730981";
-    public static final String WIDGET_VALUE_BAROMETER = "730982";
-    public static final String WIDGET_VALUE_LUXOMETER = "730983";
+    private static final MediaType JSON = MediaType.parse("application/json; charset=utf-8");
+    private static final String API_KEY = "dUpDbnYxWG1hdlFPejVBUXhPaXBrTXpBd1phbzBXeUZ4VlNlamludXhqQnFpZnplMEQ6Z2FuenUxM0dU";
+    private static final String JSON_PARAM_TIMESTAMP = "timestamp";
+    private static final String JSON_PARAM_VALUE = "value";
+    private static final String API_PUSH_URL = "https://push.ducksboard.com/v/";
 
-    public static final String WIDGET_GRAPHIC_TEMPERATURE = "730985";
-    public static final String WIDGET_GRAPHIC_TEMPERATURE_IR = "730986";
-    public static final String WIDGET_GRAPHIC_HUMIDITY = "730988";
-    public static final String WIDGET_GRAPHIC_BAROMETER = "730989";
-    public static final String WIDGET_GRAPHIC_LUXOMETER = "730990";
+    /*
+     * Se permitirá hacer push a los valores cada 3" y a las gráficas cada 60"
+     */
+    private static final long WIDGET_VALUE_PUT_INTERVAL = 3000;
+    private static final long WIDGET_GRAPHIC_PUT_INTERVAL = 60000;
+
+    private static final String WIDGET_VALUE_TEMPERATURE = "730979";
+    private static final String WIDGET_VALUE_TEMPERATURE_IR = "730980";
+    private static final String WIDGET_VALUE_HUMIDITY = "730981";
+    private static final String WIDGET_VALUE_BAROMETER = "730982";
+    private static final String WIDGET_VALUE_LUXOMETER = "730983";
+
+    private static final String WIDGET_GRAPHIC_TEMPERATURE = "730985";
+    private static final String WIDGET_GRAPHIC_TEMPERATURE_IR = "730986";
+    private static final String WIDGET_GRAPHIC_HUMIDITY = "730988";
+    private static final String WIDGET_GRAPHIC_BAROMETER = "730989";
+    private static final String WIDGET_GRAPHIC_LUXOMETER = "730990";
 
 
+    private long _lastPutTemperatureValue;
+    private long _lastPutTemperatureIRValue;
+    private long _lastPutHumidityValue;
+    private long _lastPutBarometerValue;
+    private long _lastPutLuxometerValue;
 
+    private long _lastPutTemperatureGraphic;
+    private long _lastPutTemperatureIRGraphic;
+    private long _lastPutHumidityGraphic;
+    private long _lastPutBarometerGraphic;
+    private long _lastPutLuxometerGraphic;
 
     private static DucksboardController ourInstance = new DucksboardController();
 
@@ -51,55 +67,63 @@ public class DucksboardController {
     }
 
     public void putTemperatureValue(Double value, long timepstamp) {
-        putDashboardValue(WIDGET_VALUE_TEMPERATURE, value, timepstamp);
-        putDashboardValue(WIDGET_GRAPHIC_TEMPERATURE, value, timepstamp);
+        _lastPutTemperatureValue = putDashboardValue(WIDGET_VALUE_TEMPERATURE, value, timepstamp, _lastPutTemperatureValue, WIDGET_VALUE_PUT_INTERVAL);
+        _lastPutTemperatureGraphic = putDashboardValue(WIDGET_GRAPHIC_TEMPERATURE, value, timepstamp, _lastPutTemperatureGraphic, WIDGET_GRAPHIC_PUT_INTERVAL);
     }
 
     public void putTemperatureIRValue(Double value, long timepstamp) {
-        putDashboardValue(WIDGET_VALUE_TEMPERATURE_IR, value, timepstamp);
-        putDashboardValue(WIDGET_GRAPHIC_TEMPERATURE_IR, value, timepstamp);
+        _lastPutTemperatureIRValue = putDashboardValue(WIDGET_VALUE_TEMPERATURE_IR, value, timepstamp, _lastPutTemperatureIRValue, WIDGET_VALUE_PUT_INTERVAL);
+        _lastPutTemperatureIRGraphic = putDashboardValue(WIDGET_GRAPHIC_TEMPERATURE_IR, value, timepstamp, _lastPutTemperatureIRGraphic, WIDGET_GRAPHIC_PUT_INTERVAL);
     }
 
     public void putHumidityValue(Double value, long timepstamp) {
-        putDashboardValue(WIDGET_VALUE_HUMIDITY, value, timepstamp);
-        putDashboardValue(WIDGET_GRAPHIC_HUMIDITY, value, timepstamp);
+        _lastPutHumidityValue = putDashboardValue(WIDGET_VALUE_HUMIDITY, value, timepstamp, _lastPutHumidityValue, WIDGET_VALUE_PUT_INTERVAL);
+        _lastPutHumidityGraphic = putDashboardValue(WIDGET_GRAPHIC_HUMIDITY, value, timepstamp, _lastPutHumidityGraphic, WIDGET_GRAPHIC_PUT_INTERVAL);
     }
 
     public void putBarometerValue(Double value, long timepstamp) {
-        putDashboardValue(WIDGET_VALUE_BAROMETER, value, timepstamp);
-        putDashboardValue(WIDGET_GRAPHIC_BAROMETER, value, timepstamp);
+        _lastPutBarometerValue = putDashboardValue(WIDGET_VALUE_BAROMETER, value, timepstamp, _lastPutBarometerValue, WIDGET_VALUE_PUT_INTERVAL);
+        _lastPutBarometerGraphic = putDashboardValue(WIDGET_GRAPHIC_BAROMETER, value, timepstamp, _lastPutBarometerGraphic, WIDGET_GRAPHIC_PUT_INTERVAL);
     }
 
     public void putLuxometerValue(Double value, long timepstamp) {
-        putDashboardValue(WIDGET_VALUE_LUXOMETER, value, timepstamp);
-        putDashboardValue(WIDGET_GRAPHIC_LUXOMETER, value, timepstamp);
+        _lastPutLuxometerValue = putDashboardValue(WIDGET_VALUE_LUXOMETER, value, timepstamp, _lastPutLuxometerValue, WIDGET_VALUE_PUT_INTERVAL);
+        _lastPutLuxometerGraphic = putDashboardValue(WIDGET_GRAPHIC_LUXOMETER, value, timepstamp, _lastPutLuxometerGraphic, WIDGET_GRAPHIC_PUT_INTERVAL);
     }
 
-    private void putDashboardValue(final String widgetID, final double value, final long timestamp) {
-        Thread sendThread = new Thread(new Runnable() {
-            @Override
-            public void run() {
-                try {
-                    JSONObject jsonObject = new JSONObject();
-                    jsonObject.put(JSON_PARAM_VALUE, value);
-                    jsonObject.put(JSON_PARAM_TIMESTAMP, timestamp);
-                    String json = jsonObject.toString();
-                    String url = API_PUSH_URL + widgetID;
+    private long putDashboardValue(final String widgetID, final double value, final long timestamp, long lastPutTimestamp, long putInterval) {
+        long currentTime = System.currentTimeMillis();
+        long nextPutAllowed = lastPutTimestamp + putInterval;
 
-                    OkHttpClient _httpClient = new OkHttpClient();
-                    RequestBody body = RequestBody.create(JSON, json);
-                    Request request = new Request.Builder()
-                            .url(url)
-                            .post(body)
-                            .header("Authorization", "Basic " + API_KEY)
-                            .build();
+        if (currentTime >= nextPutAllowed) {
+            Thread sendThread = new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        JSONObject jsonObject = new JSONObject();
+                        jsonObject.put(JSON_PARAM_VALUE, value);
+                        jsonObject.put(JSON_PARAM_TIMESTAMP, timestamp);
+                        String json = jsonObject.toString();
+                        String url = API_PUSH_URL + widgetID;
 
-                    _httpClient.newCall(request).execute();
-                } catch (Exception e) {
-                    e.printStackTrace();
+                        OkHttpClient _httpClient = new OkHttpClient();
+                        RequestBody body = RequestBody.create(JSON, json);
+                        Request request = new Request.Builder()
+                                .url(url)
+                                .post(body)
+                                .header("Authorization", "Basic " + API_KEY)
+                                .build();
+
+                        _httpClient.newCall(request).execute();
+                    } catch (Exception e) {
+                        e.printStackTrace();
+                    }
                 }
-            }
-        });
-        sendThread.start();
+            });
+            sendThread.start();
+            return currentTime;
+        } else {
+            return lastPutTimestamp;
+        }
     }
 }
